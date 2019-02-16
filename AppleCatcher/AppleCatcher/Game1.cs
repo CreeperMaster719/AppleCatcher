f4deccef3463b9f1dd0957bf9517d68ee4842fe5
Eliot Troop <etroop719@gmail.com>
2019-02-16 11:05:05 -0800

    stuff

--- AppleCatcher/AppleCatcher/Game1.cs
+++ AppleCatcher/AppleCatcher/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace AppleCatcher
 {
@@ -15,6 +16,9 @@ namespace AppleCatcher
         FallingItems testItem;
         Random position;
         ItemCatcher testCatcher;
+        List <FallingItems> goodItems;
+        int score = 0;
+        SpriteFont font;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -25,22 +29,28 @@ namespace AppleCatcher
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            graphics.PreferredBackBufferHeight = 720;
-            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 980;
             graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            goodItems = new List<FallingItems>();
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Vector2 testPosition = new Vector2(100, 50);
             Texture2D testTexture = Content.Load <Texture2D>("Test_Item");
             Color testTint = Color.White;
-            testItem = new FallingItems(testPosition, testTexture, testTint);
-            Vector2 testPositionC = new Vector2(200, 550);
+            for(int i = 0; i < 20; i++)
+            {
+                goodItems.Add(new FallingItems(testPosition, testTexture, testTint));
+            }
+
+            Vector2 testPositionC = new Vector2(200, 860);
             Texture2D testTextureC = Content.Load<Texture2D>("Test_Catcher");
+            font = Content.Load<SpriteFont>("Font");
             Color testTintC = Color.White;
             testCatcher = new ItemCatcher(testPositionC, testTextureC, testTintC);
             // TODO: use this.Content to load your game content here
@@ -52,16 +62,9 @@ namespace AppleCatcher
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            testItem.position.Y += 5;
             position = new Random();
-            if(testItem.position.Y + testItem.texture.Height> GraphicsDevice.Viewport.Height)
-            {
-                //testItem.position.Y = 0;
-                // testItem.position.X = position.Next(0, GraphicsDevice.Viewport.Width);
-                testItem.position.Y += -5;
-            }
 
-            testCatcher.Update(testItem, GraphicsDevice.Viewport, position, Keys.A, Keys.D);
+            testCatcher.Update(goodItems, GraphicsDevice.Viewport, position, Keys.A, Keys.D);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -71,7 +74,11 @@ namespace AppleCatcher
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            testItem.Draw(spriteBatch);
+            for(int i = 0; i < goodItems.Count; i++)
+            {
+                goodItems[i].Draw(spriteBatch);
+            }
+            spriteBatch.DrawString(font, $"{testCatcher.Score}", new Vector2(100, 100), Color.Orange);
             testCatcher.Draw(spriteBatch);
             // TODO: Add your drawing code here
             spriteBatch.End();
--- AppleCatcher/AppleCatcher/ItemCatcher.cs
+++ AppleCatcher/AppleCatcher/ItemCatcher.cs
@@ -12,15 +12,16 @@ namespace AppleCatcher
     class ItemCatcher : Sprite
     {
         int xSpeed = 1;
+        public int Score;
 
         public ItemCatcher(Vector2 vector2, Texture2D texture2D, Color tint)
                 : base(vector2, texture2D, tint)
         {
-
+            Score = 0;
         }
 
 
-        public void Update(FallingItems testitem, Viewport viewport, Random random, Keys keyLeft, Keys keyRight)
+        public void Update(List<FallingItems> items, Viewport viewport, Random random, Keys keyLeft, Keys keyRight)
         {
           
             KeyboardState keyboard = Keyboard.GetState();
@@ -33,15 +34,37 @@ namespace AppleCatcher
             {
                 position.X += 15;
             }
-            if(position.X + texture.Width > viewport.Width)
+            if(position.X + texture.Width >= viewport.Width + 50)
             {
-                position.X = 0;
+                position.X += -5;
             }
-            if (HitBox.Intersects(testitem.HitBox))
+            else if(position.X < 0)
             {
-                testitem.position.Y = 0;
-                testitem.position.X = random.Next(0, viewport.Width);
+                position.X += 5;
             }
+            for(int i = 0; i < items.Count; i++)
+            {
+                FallingItems item = items[i];
+                if (HitBox.Intersects(item.HitBox))
+                {
+                    Score++;
+                    item.position.Y = -100;
+                    item.position.X = random.Next(0, viewport.Width - item.texture.Width);
+                }
+
+                item.position.Y += 15;
+                
+                if (item.position.Y + item.texture.Height > viewport.Height + 100)
+                {
+                    //item.position.Y = 0;
+                    // item.position.X = position.Next(0, GraphicsDevice.Viewport.Width);
+                    item.position.Y = -100;
+                }
+            }
+
+            
+            
+
         }
     }
 }
